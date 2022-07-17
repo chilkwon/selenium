52885ab678e52ac90b5e8b36352650741b70c6bd
Chil's Coding World <mbsrkch@gmail.com>
2022-07-16 21:56:39 -0400

    Added password negative test method and created test suite for postive and negative tests. Updated pom.xml for test suites

--- src/test/java/com/herokuapp/theinternet/NegativeTests.java
+++ src/test/java/com/herokuapp/theinternet/NegativeTests.java
@@ -10,7 +10,7 @@ import org.testng.annotations.Test;
 public class NegativeTests {
 
 	@Test
-	public void negativeLoginTest() {
+	public void incorrectUserNameTest() {
 
 		System.out.println("Starting login negative test");
 		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
@@ -46,4 +46,40 @@ public class NegativeTests {
 
 	}
 
+	@Test
+	public void incorrectPasswordTest() {
+
+		System.out.println("Starting login negative test");
+		System.setProperty("webdriver.chrome.driver", "src/main/resources/chromedriver.exe");
+		WebDriver chromDriver = new ChromeDriver();
+		chromDriver.manage().window().maximize();
+
+		String url = "https://the-internet.herokuapp.com/login";
+		chromDriver.get(url);
+		System.out.println("Login Page is opened");
+
+		WebElement userName = chromDriver.findElement(By.id("username"));
+		userName.sendKeys("tomsmith");
+//		enter password
+		WebElement password = chromDriver.findElement(By.name("password"));
+		password.sendKeys("asdf");
+
+		WebElement loginButton = chromDriver.findElement(By.tagName("button"));
+		loginButton.click();
+
+		// Verification
+
+		WebElement errorMsg = chromDriver.findElement(By.id("flash"));
+		String expectedUsrMsg = "Your password is invalid!";
+		String actualErrorMsg = errorMsg.getText();
+
+		Assert.assertTrue(actualErrorMsg.contains(expectedUsrMsg),
+				"Actual error message does not contain expected. \nActual: " + actualErrorMsg + "\nExpected:"
+						+ expectedUsrMsg);
+		
+		//close browser
+
+		chromDriver.quit();
+
+	}
 }
